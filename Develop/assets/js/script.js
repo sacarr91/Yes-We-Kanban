48d8e46d136116b9488fa55be82ff7d09f198432
Stephanie Carr <sacarr91@gmail.com>
2024-06-17 19:51:03 -0400

    did too much work before committing--oops. Googled how to generate a task ID & utilize persistent storage. Adapted code used in project 2 for HTML injection and modal creation to start coding new task composition

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,28 +2,71 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+const composeTask = () => {
+     const modalAdd = `<div class="modal fade" id="staticBackdrop-login" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Log In</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form class="form login-form">
+                                <div class="form-group">
+                                    <div>
+                                        <label for="email">Email Address</label>
+                                        <input type="email" class="form-control" id="email" placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="password" placeholder="Password">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary btn-login submit" id="login-btn">Log In</button>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+     cardDeck.innerHTML += card;
+    }
+}
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    nextId++;
+    localStorage.setItem(`nextId`, nextId);
+    return nextId;
 }
 
 // Todo: create a function to create a task card
+// HTML injection
 function createTaskCard(task) {
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
+// pull tasks from location
+// add current tasks
+// make cards draggable
 function renderTaskList() {
+    //jquery widget -- UI interactions
+    taskList
 
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
+function handleAddTask(event) {
+    let newTask = {
+        title: 
+    }
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -34,7 +77,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-renderTaskList();
+    renderTaskList();
 
 });
 
