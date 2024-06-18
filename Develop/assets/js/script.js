faa0c185dfc8a70138c234990dc63ac9e579bbfc
Stephanie Carr <sacarr91@gmail.com>
2024-06-18 11:25:52 -0400

    I think I solved my tasks array init issue praisethalort

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,13 +1,15 @@
 let taskList = [];
 // Retrieve tasks and nextId from localStorage
 localStorage.getItem("tasks") === null
-    ? console.log("No tasks yet")
+    ? localStorage.setItem("tasks", (JSON.stringify(taskList)))
     : console.log(localStorage.getItem("tasks"));
 
 localStorage.getItem("nextId") === null
-    ? localStorage.setItem("nextId", 0)
+    ? localStorage.setItem("nextId", 1)
     // : nextId = JSON.parse(localStorage.getItem("nextId"));
-    : nextId = localStorage.getItem("nextId");
+    : console.log("nextId is established");
+
+let nextId = localStorage.getItem("nextId");
 
 const newTaskTitleEl = document.getElementById("newTaskTitle");
 const newTaskDetailsEl = document.getElementById("newTaskDetails");
@@ -67,11 +69,11 @@ function createTaskCard(task) {
                   <div class="card-body">
                     <h6 class="card-title">${task.details}</h6>
                     <p class="card-text">${task.due}</p>
-                    <a href="#" class="btn btn-danger" id="deleteTaskBtn">Delete</a>
+                    <a href="#" class="btn btn-danger" id="deleteTaskBtn" taskId="${task.id}">Delete</a>
                   </div>
                 </div>`
     todoCards.innerHTML += card;
-    const deleteTaskBtn = document.querySelectorAll("#deleteTaskBtn");
+    let deleteTaskBtn = document.querySelectorAll("#deleteTaskBtn");
     deleteTaskBtn.addEventListener("click", handleDeleteTask);
 }
 
@@ -82,26 +84,40 @@ function createTaskCard(task) {
 function renderTaskList() {
     //make columns & cards appear from localStorage
     // taskList = JSON.parse(localStorage.getItem("tasks"));
- // `${task.status}` still exists
+    // `${task.status}` still exists
 }
 
 // TODO: create a function to handle deleting a task
 function handleDeleteTask(event) {
     //delete card by ID taskCard${task.id}
+    let deleteId = event.target.getAttribute(taskId);
+    delete taskList[deleteId];
+    let deleteEl = document.getElementById(`taskCard${deleteId}`);
+    deleteEl.remove();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) { // handle MOVE
     //update status of task
- // `${task.status}` still exists
+    // ${task.status}
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     renderTaskList();
-    taskSubmitBtn.addEventListener("click", composeTask);
-    const deleteTaskBtn = document.querySelectorAll("#deleteTaskBtn");
-    deleteTaskBtn.addEventListener("click", handleDeleteTask);
+    addListeners();
+
+    // jQuery date picker script is in HTML code
 });
+
+const addListeners = () => {
+    taskSubmitBtn.addEventListener("click", composeTask);
+
+    let deleteBtns = document.querySelectorAll("#deleteTaskBtn");
+    deleteBtns.forEach(button => {
+        button.addEventListener("click", handleDeleteTask);
+    });
+
+};
 
 dayjs().format()
